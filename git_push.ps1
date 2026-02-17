98dd297dc5d260e14c7fda627f76abfb37d96540
Віктор <buriak.viktor17@gmail.com>
2026-02-18 01:01:46 +0200

    fix: add ImGui as regular vendor files (not submodule)

--- git_push.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location 'c:\Programs\Programming\PythonProject\ProtoEngine'
-git add -A
-git status --short
-git commit -m "feat: ImGui integration, Timer, Camera refactor, Voxel World system (Culled Meshing)"
-git push origin main
-Write-Host "=== DONE ==="
-git log --oneline -3
